f5e1307058ce7dd26eeb193fcee42d1836ed9dbd
henry <henryadebayo250@gmail.com>
2022-09-27 02:13:39 +0100

    Created color file and custom textstyle file ⚒️

---
+++ lib/Utils/App_textStyles/app_textstyle_file.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle Whitelight = TextStyle(
+      //fontWeight: FontWeight.w300,
+      color: Colors.white);
+
+  static const TextStyle WhiteMedium =
+      TextStyle(fontWeight: FontWeight.w500, color: Colors.white);
+
+  static const TextStyle WhiteBold =
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
+
+  static const TextStyle Blacklight = TextStyle(
+      //fontWeight: FontWeight.w300,
+      color: Colors.black);
+
+  static const TextStyle BlackMedium =
+      TextStyle(fontWeight: FontWeight.w500, color: Colors.black);
+
+  static const TextStyle BlackBold =
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.black);
+
+  static const TextStyle Bluelight = TextStyle(
+      //fontWeight: FontWeight.w300,
+      color: Colors.black);
+
+  static const TextStyle BlueMedium =
+      TextStyle(fontWeight: FontWeight.w500, color: Colors.black);
+
+  static const TextStyle BlueBold =
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.black);
+}
